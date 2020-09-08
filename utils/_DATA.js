c04d737cc26c75ccc0db698867d60cabbb54b995
Noha M <nm1488@fayoum.edu.eg>
2020-09-08 19:45:12 +0200

    Added Stored DATA

---
+++ utils/_DATA.js
@@ -0,0 +1,29 @@
+export const decksData = {
+  
+    React: {
+      title: 'React',
+      questions: [
+        {
+          question: 'What is React?',
+          answer: 'A library for managing user interfaces',
+          correctAnswer: 'false',
+        },
+        {
+          question: 'Where do you make Ajax requests in React?',
+          answer: 'The componentDidMount lifecycle event',
+          correctAnswer: 'false',
+        }
+      ]
+    },
+  
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer: 'The combination of a function and the lexical environment within which that function was declared.',
+          correctAnswer: 'true'
+        }
+      ]
+    }
+  }

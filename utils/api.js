e7a71914efe8a34ca84c2dbfebea7b8d3a5b9c83
Noha M <nm1488@fayoum.edu.eg>
2020-09-08 19:50:15 +0200

    added Api methods && colors

---
+++ utils/api.js
@@ -0,0 +1,89 @@
+import { AsyncStorage } from 'react-native'
+import { decksData } from './_DATA'
+
+import { AsyncStorage } from 'react-native';
+import {decksData} from './_DATA'
+
+const FLASHCARDS_STORAGE_KEY = 'MobileFlashcards:decks'
+
+export const getInitialData = () => {
+  //a func to return all of the decks along with their titles, questions, and answers.
+  return  decksData
+} 
+
+// getDecks: return all of the decks along with their titles, questions, and answers.
+export function getDecks() {
+  return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
+  .then(results => {
+    if(results === null) {
+      AsyncStorage.setItem(FLASHCARDS_STORAGE_KEY, JSON.stringify(decksData))
+      return decksData
+    } else {
+      return JSON.parse(results)
+    }
+  })
+}
+
+// getDeck: take in a single id argument and return the deck associated with that id.
+
+export async function getDeck(id) {
+  try {
+    const results = await AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
+
+    return JSON.parse(results)[id]
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// saveDeckTitle: take in a single title argument and add it to the decks.
+export function saveDeckTitle(title) {
+    return AsyncStorage.mergeItem(
+      FLASHCARDS_STORAGE_KEY, 
+      JSON.stringify({
+      [title]: {
+        title,
+        questions: [],
+      }
+    }))
+     
+}
+
+export async function removeDeck(key) {
+  try {
+    const results = await AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
+    const data = JSON.parse(results)
+    data[key] = undefined
+    delete data[key]
+    AsyncStorage.setItem(FLASHCARDS_STORAGE_KEY, JSON.stringify(data))
+  } catch(err) {
+    console.log(err)
+  }
+}
+// addCardToDeck: take in two arguments, title and card, and will add the card to the list of questions for the deck with the associated title
+
+export async function addCardToDeck(title, card) {
+  try {
+    const deck = await getDeck(title)
+
+    await AsyncStorage.mergeItem(
+      FLASHCARDS_STORAGE_KEY,
+      JSON.stringify({
+        [title]: {
+          questions: [...deck.questions].concat(card)
+        }
+      })
+    )
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function reset() {
+    try {
+        await AsyncStorage.removeItem(FLASHCARDS_STORAGE_KEY)
+
+    } catch (err) {
+        console.log(err)
+    }
+}
---
+++ utils/colors.js
@@ -0,0 +1,6 @@
+export const blue = '#0077FF'
+export const nile = '#00C3FF'
+export const orange = '#FE7235'
+export const yellow = '#FEA735'
+export const white = '#FCF5EF'
+
